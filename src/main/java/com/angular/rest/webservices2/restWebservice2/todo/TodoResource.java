85087d2523f2fa9f39453957b6b520fe876af35f
Saurabh <emosrb@gmail.com>
2021-01-17 23:01:02 +0530

    added crossOrigin to TodoResource  controller class

--- src/main/java/com/angular/rest/webservices2/restWebservice2/todo/TodoResource.java
+++ src/main/java/com/angular/rest/webservices2/restWebservice2/todo/TodoResource.java
@@ -1,12 +1,14 @@
 package com.angular.rest.webservices2.restWebservice2.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class TodoResource {
 
