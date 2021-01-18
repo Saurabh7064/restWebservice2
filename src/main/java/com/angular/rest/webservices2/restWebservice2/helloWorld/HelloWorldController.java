af9b990a8ad8f08fa1964db3e430a4074f29575b
Saurabh <emosrb@gmail.com>
2021-01-18 08:18:10 +0530

    added Delete API

--- src/main/java/com/angular/rest/webservices2/restWebservice2/helloWorld/HelloWorldController.java
+++ src/main/java/com/angular/rest/webservices2/restWebservice2/helloWorld/HelloWorldController.java
@@ -24,4 +24,6 @@ public class HelloWorldController {
         public HelloWorldBean helloWorldPathVariable(@PathVariable String name ){
             return new HelloWorldBean (String.format("Hello World,%s",name));
         }
+
+
     }
